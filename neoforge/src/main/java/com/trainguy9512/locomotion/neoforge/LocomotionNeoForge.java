7452b9fc407a2240b9002576c9d7bc3e994ec326
James Pelter <trainguy9512@gmail.com>
2025-04-01 14:43:56 -0500

    Neoforge main class (does not work yet)

---
+++ neoforge/src/main/java/com/trainguy9512/locomotion/neoforge/LocomotionNeoForge.java
@@ -0,0 +1,11 @@
+package com.trainguy9512.locomotion.neoforge;
+
+import com.trainguy9512.locomotion.LocomotionMain;
+import net.neoforged.fml.common.Mod;
+
+@Mod(LocomotionMain.MOD_ID)
+public class LocomotionNeoForge {
+    public LocomotionNeoForge() {
+        LocomotionMain.initialize();
+    }
+}
